3574ea04c4fb0e12cf225e5bdbec2dc78055b40b
brent hartshorn <brenthartshorn@proton.me>
2026-06-16 18:30:32 -0700

    micropython part III

--- tests/frontend_tests/error_lexer.c
+++ tests/frontend_tests/error_lexer.c
@@ -1,7 +1,5 @@
 // error: expected "FILENAME" or <FILENAME> after include directive
 #include
-// error: expected "FILENAME" or <FILENAME> after include directive
-#include blah
 // error: missing terminating character for include filename
 #include <hey there
 // error: missing terminating character for include filename
