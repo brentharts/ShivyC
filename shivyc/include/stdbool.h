5ab9c0cfff95ed0ca121180a9ee502e8d449db80
brent hartshorn <brenthartshorn@proton.me>
2026-06-18 09:49:04 -0700

    fixes true/false

--- shivyc/include/stdbool.h
+++ shivyc/include/stdbool.h
@@ -1,2 +1,7 @@
-// TODO: define bool, true, and false as macros
+#ifndef _STDBOOL_H
+#define _STDBOOL_H
 typedef _Bool bool;
+#define true 1
+#define false 0
+#define __bool_true_false_are_defined 1
+#endif
