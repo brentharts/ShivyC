680c7c17a07deeebce49139b9dc4e002c6ff8fe9
brent hartshorn <brenthartshorn@proton.me>
2026-07-16 15:46:19 -0700

    minibrowser baremetal part I

---
+++ examples/rpython2c/mbos/rpy/freestanding_inc/ctype.h
@@ -0,0 +1,13 @@
+/* Freestanding <ctype.h> shadow. */
+#ifndef MBOS_FS_CTYPE_H
+#define MBOS_FS_CTYPE_H
+int isspace(int c);
+int isdigit(int c);
+int isalpha(int c);
+int isalnum(int c);
+int isupper(int c);
+int islower(int c);
+int isprint(int c);
+int toupper(int c);
+int tolower(int c);
+#endif
